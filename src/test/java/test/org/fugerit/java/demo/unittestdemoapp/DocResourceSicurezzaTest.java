166b922d4871ecf2f0d0cb8e31cd18c0efd16e0d
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-11-25 22:54:56 +0100

    feat: junit tags "security", "authorized", "unauthorized", "forbidden"

--- src/test/java/test/org/fugerit/java/demo/unittestdemoapp/DocResourceSicurezzaTest.java
+++ src/test/java/test/org/fugerit/java/demo/unittestdemoapp/DocResourceSicurezzaTest.java
@@ -2,6 +2,7 @@ package test.org.fugerit.java.demo.unittestdemoapp;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,6 +12,8 @@ import static org.hamcrest.Matchers.containsString;
 class DocResourceSicurezzaTest {
 
     @Test
+    @Tag("security")
+    @Tag("authorized")
     void testHtmlOkNoAdminRole() {
         // a questo path sono autorizzati anche gli utenti con semplice ruolo 'user'
         given()
@@ -19,6 +22,8 @@ class DocResourceSicurezzaTest {
     }
 
     @Test
+    @Tag("security")
+    @Tag("unauthorized")
     void testInvalidJwt() {
         given()
                 .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJVU0VSMSIsIm5hbWUi")
@@ -27,6 +32,8 @@ class DocResourceSicurezzaTest {
     }
 
     @Test
+    @Tag("security")
+    @Tag("unauthorized")
     void testInvalidJwtPayload() {
         given()
                 .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJVU0VSMSIsIm5hbWUi.signature")
@@ -35,12 +42,16 @@ class DocResourceSicurezzaTest {
     }
 
     @Test
+    @Tag("security")
+    @Tag("unauthorized")
     void testMarkdown401NoAuthorizationBearer() {
         given()
                 .when().get("/doc/example.md").then().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
     @Test
+    @Tag("security")
+    @Tag("forbidden")
     void testMarkdown403NoAdminRole() {
         given()
                 .header("Authorization", "Bearer " + DocResourceTest.JWT_USER2)
@@ -48,12 +59,16 @@ class DocResourceSicurezzaTest {
     }
 
     @Test
+    @Tag("security")
+    @Tag("unauthorized")
     void testHtml401NoAuthorizationBearer() {
         given()
                 .when().get("/doc/example.html").then().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
     @Test
+    @Tag("security")
+    @Tag("unauthorized")
     void testAsciiDocNoAuthorizationBearer() {
         given()
                 .when().get("/doc/example.adoc").then().statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
