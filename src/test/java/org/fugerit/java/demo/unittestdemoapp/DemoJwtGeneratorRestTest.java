658805af2ef56cf5041c4260af6c0374368d6b63
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2026-02-15 18:33:21 +0100

    fix: demo test

--- src/test/java/org/fugerit/java/demo/unittestdemoapp/DemoJwtGeneratorRestTest.java
+++ src/test/java/org/fugerit/java/demo/unittestdemoapp/DemoJwtGeneratorRestTest.java
@@ -1,11 +1,13 @@
 package org.fugerit.java.demo.unittestdemoapp;
 
+import io.quarkus.test.junit.QuarkusTest;
 import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
+@QuarkusTest
 class DemoJwtGeneratorRestTest {
 
     @Test
